dfb7829f59b1a7be270899bf7f76e182c513716c
Morbo-ui <lbvf6000@gmail.com>
2021-11-08 20:21:29 +0300

    Initial commit

---
+++ src/test/java/ru/morboui/boocktrackerdataloader/BoockTrackerDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.morboui.boocktrackerdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoockTrackerDataLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
